bbedf29da2ca39247b94526c4781ffb66407588a
ercan cimen <ercancimen3434@gmail.com>
2020-03-15 17:22:43 +0300

    version1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "products",
+        pathMatch: "full"
+      },
+      {
+        path: "products",
+        component: ProductComponent
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
